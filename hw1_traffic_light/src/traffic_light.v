4aa9d71de619e20f166c348717c57722c139371a
cicero884 <joy861225@fmail.com>
2021-10-22 10:21:15 +0800

    fix cycle bug and add gitignore

--- hw1_traffic_light/src/traffic_light.v
+++ hw1_traffic_light/src/traffic_light.v
@@ -2,42 +2,26 @@ module traffic_light (
   input  clk,
   input  rst,
   input  pass,
-  output R,
-  output G,
-  output Y
+  output reg R,
+  output reg G,
+  output reg Y
 );
 
-reg [7]devide_clock;
-reg [5]status_cnt;
-reg pass_triggered;
-always@(posedge clk,posedge rst,posedge pass_triggered) begin
-	if(rst) devide_clock<=7'd0;
-	else devide_clock<=(pass_triggered)? 7'd0:devide_clock+1;
-end
-
-always@(negedge devide_clock[6],posedge rst,posedge pass) begin
-	if(rst) begin
-		pass_triggered<=1'b0;
-		status_cnt<=1'b0;
-	else begin
-		if(pass&&status_cnt>5'd10) begin
-			pass_triggered<=1'b1;
-			status_cnt<=5'd0;
-		end
-		else begin
-			pass_triggered=1<1'b0;
-			status_cnt<=(status_cnt<5'd24)? status_cnt+1:0;
-		end
-	end
+reg [11:0]clk_cnt;
+always@(posedge clk,posedge rst) begin
+	if(rst) clk_cnt<=12'd0;
+	else clk_cnt<=((pass&&clk_cnt>=12'd1024) || clk_cnt>=12'd3071)? 7'd0:clk_cnt+1;
 end
 
 always@(*) begin
 	R=1'b0;
 	G=1'b0;
 	Y=1'b0;
-	if(status_cnt<5'd10||status_cnt==5'd11||status_cnt==5'd13) G=1'b1;
-	else if(status_cnt>=5'd14&&status_cnt<5'd18) Y=1'b1;
-	else if(status_cnt>=5'd18&&status_cnt<=5'd24) R=1'b1;
+	if(clk_cnt<12'd1024||
+			(clk_cnt>=12'd1152&&clk_cnt<12'd1280)||
+			(clk_cnt>=12'd1408&&clk_cnt<12'd1536)) G=1'b1;
+	else if(clk_cnt>=12'd1536&&clk_cnt<12'd2048) Y=1'b1;
+	else if(clk_cnt>=12'd2048&&clk_cnt<12'd3072) R=1'b1;
 end
 
 endmodule
