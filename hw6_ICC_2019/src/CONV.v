3fe075227c419a96aef971bf198990848f5ee452
cicero884 <joy861225@fmail.com>
2022-01-06 00:11:46 +0800

    wrong fix

--- hw6_ICC_2019/src/CONV.v
+++ hw6_ICC_2019/src/CONV.v
@@ -1,16 +1,16 @@
 `define SIDE_MAX {~6'd0}
 
-`define MAX_2(a,b) {(a>b)? a:b}
-`define MUL(a,b) {a*b}[4:23]
+`define MAX_2(a,b) ((a>b)? a:b)
+`define MUL(a,b) (a*b)
 `define KER0_CONV \
-	{`MUL(tmp[0][0],20'h0A89E)+`MUL(tmp[0][1],20'h092D5)+`MUL(tmp[0][2],20'h06D43)+ \
-	`MUL(tmp[1][0],20'h01004)+`MUL(tmp[1][1],20'hF8F71)+`MUL(tmp[1][2],20'hF6E54)+ \
-	`MUL(tmp[2][0],20'hFA6D7)+`MUL(tmp[2][1],20'hFC834)+`MUL(tmp[2][2],20'hFAC19)}
+	(`MUL(image[caddr_wr[11:6]+0][caddr_wr[5:0]+0],20'h0A89E)+`MUL(image[caddr_wr[11:6]+0][caddr_wr[5:0]+1],20'h092D5)+`MUL(image[caddr_wr[11:6]+0][caddr_wr[5:0]+2],20'h06D43)+ \
+	`MUL(image[caddr_wr[11:6]+1][caddr_wr[5:0]+0],20'h01004)+`MUL(image[caddr_wr[11:6]+1][caddr_wr[5:0]+1],20'hF8F71)+`MUL(image[caddr_wr[11:6]+1][caddr_wr[5:0]+2],20'hF6E54)+ \
+	`MUL(image[caddr_wr[11:6]+2][caddr_wr[5:0]+0],20'hFA6D7)+`MUL(image[caddr_wr[11:6]+2][caddr_wr[5:0]+1],20'hFC834)+`MUL(image[caddr_wr[11:6]+2][caddr_wr[5:0]+2],20'hFAC19))
 
 `define KER1_CONV \
-	{`MUL(tmp[0][0],20'hFDB55)+`MUL(tmp[0][1],20'h02992)+`MUL(tmp[0][2],20'hFC994)+ \
-	`MUL(tmp[1][0],20'h050FD)+`MUL(tmp[1][1],20'h02F20)+`MUL(tmp[1][2],20'h0202D)+ \
-	`MUL(tmp[2][0],20'h03BD7)+`MUL(tmp[2][1],20'hFD369)+`MUL(tmp[2][2],20'h05E68)}
+	(`MUL(image[caddr_wr[11:6]+0][caddr_wr[5:0]+0],20'hFDB55)+`MUL(image[caddr_wr[11:6]+0][caddr_wr[5:0]+1],20'h02992)+`MUL(image[caddr_wr[11:6]+0][caddr_wr[5:0]+2],20'hFC994)+ \
+	`MUL(image[caddr_wr[11:6]+1][caddr_wr[5:0]+0],20'h050FD)+`MUL(image[caddr_wr[11:6]+1][caddr_wr[5:0]+1],20'h02F20)+`MUL(image[caddr_wr[11:6]+1][caddr_wr[5:0]+2],20'h0202D)+ \
+	`MUL(image[caddr_wr[11:6]+2][caddr_wr[5:0]+0],20'h03BD7)+`MUL(image[caddr_wr[11:6]+2][caddr_wr[5:0]+1],20'hFD369)+`MUL(image[caddr_wr[11:6]+2][caddr_wr[5:0]+2],20'h05E68))
 
 module CONV (
 	input clk,
@@ -19,53 +19,60 @@ module CONV (
 	input [19:0] idata,
 	input [19:0] cdata_rd,
 
-	output logic busy,
-	output logic crd,
-	output logic cwr,
-	output logic [2:0] csel,
-	output logic [11:0] iaddr,
-	output logic [11:0] caddr_rd,
-	output logic [11:0] caddr_wr,
-	output logic [19:0] cdata_wr
+	output reg busy,
+	output reg crd,
+	output reg cwr,
+	output reg [2:0] csel,
+	output reg [11:0] iaddr,
+	output reg [11:0] caddr_rd,
+	output reg [11:0] caddr_wr,
+	output reg [19:0] cdata_wr
 );
 
 // counter & stage
-enum [1:0]bit {CONV=2'd2,POOL=2'd1,FLAT=2'd0} stage;
+//enum [1:0]bit {CONV=2'd2,POOL=2'd1,FLAT=2'd0} stage;
+reg [1:0] stage;
 reg [19:0]tmp[2:0][2:0];
-logic sync_req;
-logic is_kernel0;
-logic r_last;
-assign r_last=(reg_row==reg_MAX);
-always_ff @(posedge clk,posedge reset) begin
+reg [19:0]image[65:0][65:0];
+reg sync_req;
+reg is_kernel0;
+wire r_last;
+reg [1:0]reg_row;
+reg [1:0]reg_col;
+reg [1:0]reg_MAX;
+integer i;
+assign r_last=(reg_row==reg_MAX&&reg_col==reg_MAX);
+always @(posedge clk,posedge reset) begin
 	if(reset) begin
-		tmp<=9'd0;
-		stage<=CONV;
-		sync_req<=1'b1;
+		stage<=2'd3;
+		sync_req<=1'b0;
 		is_kernel0<=1'b0;
+		caddr_wr<=12'd0;
+		cwr<=1'b0;
+		iaddr<=12'd0;
+		busy<=1'b0;
+		for(i=0;i<66;i=i+1) begin
+			image[0][i]<=20'd0;
+			image[65][i]<=20'd0;
+			image[i][0]<=20'd0;
+			image[i][65]<=20'd0;
+		end
 	end
 	else begin
 		case(stage)
-			CONV:begin
+			2'd3:begin //request image
 				busy<=1'b1;
-				case(1'b1)
-					r_last:begin //prepare write ker0;
-						is_kernel0<=1'b1;
-						cwr<=1'b1;
-					end
-					is_kernel0:begin //prepare write ker1
-						is_kernel0<=1'b0;
-						sync_req<=1'b1;
-						cwr<=1'b1;
-					end
-					sync_req:begin
-						if(caddr_wr=={~12'd0}) stage<=POOL;
-						caddr_wr<=caddr_wr+1;
-						sync_req<=1'b0;
-						cwr<=1'b0;
-					end
-				endcase
+				if(iaddr== ~12'd0) stage<=2'd2;
+				else iaddr<=iaddr+1;
+				image[iaddr[11:6]+1][iaddr[5:0]+1]<=idata;
 			end
-			POOL:begin
+			2'd2:begin
+				if(caddr_wr=={~12'd0}) stage<=2'd1;
+				caddr_wr<=caddr_wr+1;
+				is_kernel0=~is_kernel0;
+				cwr<=1'b1;
+			end
+			2'd1:begin
 				case(1'b1)
 					r_last:begin
 						cwr<=1'b1;
@@ -73,8 +80,8 @@ always_ff @(posedge clk,posedge reset) begin
 					end
 					sync_req:begin
 						if(caddr_wr=={2'd0,{~10'd0}}) begin
-							stage<=FLAT;
-							caddr_wr<=12'd0
+							stage<=2'd0;
+							caddr_wr<=12'd0;
 						end
 						else caddr_wr<=caddr_wr+1;
 						sync_req<=1'b0;
@@ -82,7 +89,7 @@ always_ff @(posedge clk,posedge reset) begin
 					end
 				endcase
 			end
-			FLAT:begin
+			2'd0:begin
 				case(1'b1)
 					r_last:begin
 						cwr<=1'b1;
@@ -91,7 +98,7 @@ always_ff @(posedge clk,posedge reset) begin
 					sync_req:begin
 						if(caddr_wr=={1'd0,{~11'd0}}) begin
 							busy<=1'b0;
-							cwr<=1'b0
+							cwr<=1'b0;
 						end
 						else caddr_wr<=caddr_wr+1;
 						sync_req<=1'b0;
@@ -102,12 +109,8 @@ always_ff @(posedge clk,posedge reset) begin
 		endcase
 	end
 end
-// request data
-reg [1:0]reg_col;
-reg [1:0]reg_row;
-reg [1:0]reg_MAX;
-logic out_border;
-always_ff @(posedge clk,posedge reset) begin
+// request layer data
+always @(posedge clk,posedge reset) begin
 	if(reset) begin
 		crd<=1'b0;
 		reg_col<=2'd0;
@@ -128,32 +131,36 @@ always_ff @(posedge clk,posedge reset) begin
 				end
 				else reg_col<=reg_col+1;
 
-				if(out_border) tmp[reg_col][reg_row]<=20'd0;
-				else tmp[reg_col][reg_row]<=cdata_rd;
+				tmp[reg_col][reg_row]<=cdata_rd;
 			end
 		endcase
 	end
 end
 // comb circuit for output
-always_comb begin
+wire [39:0]tmp_ans;
+assign tmp_ans=(is_kernel0)? `KER0_CONV:`KER1_CONV;
+always @(*) begin
 	case(stage)
-		CONV: begin
+		2'd0: begin
+			caddr_rd=12'd0;
+			reg_MAX=2'd0;
+			cdata_wr=20'd0;
+			csel=3'b000;
+		end
+		2'd2: begin
 			caddr_rd={caddr_wr[5:0]+reg_col-1,caddr_wr[11:6]+reg_row-1};
-			out_border=((caddr_wr[5:0]==0&&reg_col==0)||(caddr_wr[5:0]==`SIDE_MAX&&reg_col==2'd2)||(caddr_wr[5:0]==0&&reg_col==0)||(caddr_wr[5:0]==`SIDE_MAX&&reg_col==2'd2));
 			reg_MAX=2'd2;
-			cdata_wr=(is_kernel0)? ((`KER0_CONV[19]) 0:`KER0_CONV):((`KER1_CONV[19]) 0:`KER1_CONV);
+			cdata_wr=(tmp_ans[39])? 0:tmp_ans[35:16];
 			csel=(is_kernel0)? 3'b001:3'b010;
 		end
-		POOL: begin
+		2'd1: begin
 			caddr_rd={{caddr_wr[4:0],1'b0}+reg_col-1,{caddr_wr[9:5],1'b0}+reg_row-1};
-			out_border=1'b0;
 			reg_MAX=2'd1;
 			cdata_wr=`MAX_2(`MAX_2(tmp[0][0],tmp[0][1]),`MAX_2(tmp[1][0],tmp[1][1]));
 			csel=(is_kernel0)? 3'b011:3'b100;
 		end
-		FLAT: begin
+		2'd0: begin
 			caddr_rd={caddr_wr[5:1],caddr_wr[10:6]};
-			out_border=1'b0;
 			reg_MAX=2'd0;
 			cdata_wr=tmp[0][0];
 			csel=3'b101;
